4ca2fd4f0a8af95e74878e71d291793f899da2db
ogagaoghene emmanuel <esavwede84@gmail.com>
2023-11-09 17:36:47 +0100

    implemented graceful shutdown

--- Src/app.js
+++ Src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -37,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
---
+++ Src/lib/graceful_shutdown/graceful_shutdown.js
@@ -0,0 +1,59 @@
+
+
+
+
+function graceful_shutdown(app)
+{
+        console.log(" Shutting Down gracefully... ") 
+
+        // db.disconnect(( err )=>{
+
+        //             if(err)
+        //             {
+        //                 console.log(" Error occured while closing db connection ")
+        //                 console.log(e)
+        //             }
+        //             else 
+        //             {
+        //                 console.log(" Db connection closed ")
+        //             }
+
+        //     app.close(( err )=>{
+
+                
+        //         if(err)
+        //         {
+        //             console.log(" Error occured while closing app ")
+        //             console.log(e)
+        //         }
+        //         else 
+        //         {
+        //             console.log(" App closed ")
+        //             process.exit(0) 
+        //         }
+
+        //     })
+
+
+        // })
+
+        app.close((err)=>{
+
+            if(err)
+            {
+                console.log(" Error occured while shutting down application ")  
+                console.log(e)  
+                process.exit(1) 
+            }
+            else 
+            {
+                console.log(" Application shutdown successfull ")
+                process.exit(0) 
+            }
+        })
+        
+} 
+
+
+
+module.exports = graceful_shutdown 
