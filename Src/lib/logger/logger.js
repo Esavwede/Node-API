ae59df8b01544626f2012d9b172a3e10d181d737
ogagaoghene emmanuel <esavwede84@gmail.com>
2023-11-14 09:46:10 +0100

    added database dependencies

--- Src/lib/logger/logger.js
+++ Src/lib/logger/logger.js
@@ -10,14 +10,7 @@ const logger = pino({
         }
     },
     timestamp: () => `,"timestamp":"${new Date().toLocaleString('en-US', { timeZone: 'Africa/Lagos' })}"`
-},  pino.transport({ targets:[ 
-    {  target: 'pino/file',
-       options: { destination: `${ __dirname }/b.log`}
-    },
-    {
-        target: 'pino/file'
-    }
-]}))
+})
 
 
 module.exports = logger 
