b67c72f80bc3682144306a0c78bf09d27440cd2e
ogagaoghene emmanuel <esavwede84@gmail.com>
2023-11-14 12:14:21 +0100

    added database connection

--- Src/lib/database/connect.js
+++ Src/lib/database/connect.js
@@ -1,2 +1,56 @@
 
-console.log("Database Connection") 
+const { options } = require('../../routes')
+const logger = require('../logger/logger') 
+const mongoose = require('mongoose') 
+
+
+function getDbURI()
+{
+    if( process.env.NODE_ENV === 'development' )
+    {
+        return process.env.MONGODB_DEV_CONNECTION_STRING
+    }
+        return process.env.MONGODB_CONNECTION_STRING 
+}
+
+
+async function connectToDB()
+{
+    try 
+    {
+
+        const dbURI = getDbURI() 
+        const db = mongoose.connection 
+        const options = { serverSelectionTimeoutMS: 60000 }
+        db.on("connecting",()=>{
+            logger.info(" Creating Database connection ", options)
+        })
+
+        db.on("connected",()=>{
+            logger.info(" Application Connected to Database ")
+        })
+
+        db.on("error",()=>{
+            logger.error(" Database Connection error ") 
+        })
+
+        db.on("disconnecting",()=>{
+            logger.warn(" Database Disconnecting ");
+        })
+        
+        db.on("disconnected",()=>{
+            logger.warn(" Database Disconnected ") 
+        })
+
+
+        await mongoose.connect( dbURI )
+      
+    }
+    catch(e)
+    {
+        logger.error(e,' Error occured while connecting to database ')
+    }
+}
+
+
+module.exports = connectToDB 
--- Src/routes/index.js
+++ Src/routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  throw new Error('Intentional Error ')
   res.render('index', { title: 'Express' });
 });
 
